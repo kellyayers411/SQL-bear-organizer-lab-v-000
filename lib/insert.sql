76e052a21e53f7f535e152e1cea83a807f28e424
K Logic <kellyayers411@gmail.com>
2018-02-06 21:08:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Rowdy", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Tabitha", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Sergeant Brown", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Melissa", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Grinch", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Wendy", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, NULL, 2, "f", "white", "sassy", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 2, "f", "white", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 2, "f", "white", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 2, "f", "white", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 2, "f", "white", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "f", "white", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 2, "f", "white", "sassy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 2, "f", "white", "sassy", 1);
