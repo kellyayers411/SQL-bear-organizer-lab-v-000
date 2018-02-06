1a195b0da02797f836d0d632fe88ac19a2fc0050
K Logic <kellyayers411@gmail.com>
2018-02-06 21:04:37 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Rowdy", 2, "f", "white", "sassy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Tabitha", 2, "f", "white", "sassy", 1)
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Sergeant Brown", 2, "f", "white", "sassy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1)
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1)
