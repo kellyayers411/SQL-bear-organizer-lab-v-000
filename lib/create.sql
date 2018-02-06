188dd4fb38260e0dedad6bd2facce2ee5dc613b0
K Logic <kellyayers411@gmail.com>
2018-02-06 21:06:24 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,3 @@
     CREATE TABLE bears ( id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive INTEGER );
     
     
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "f", "white", "sassy", 1)
-
-
-
-INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
